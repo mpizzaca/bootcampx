63b63a0b151ce82916b4e05bcd30844b52ce4dd1
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-31 17:28:38 -0400

    refactor queries to use paramaterized query to safeguard against SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,12 +9,12 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name like '%${args[0]}%'
-LIMIT ${args[1]};
-`)
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, args)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,9 +14,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, args)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
